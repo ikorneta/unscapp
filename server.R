128dd7a14685d26fb7a81010395037d13b8692b5
Iga Korneta <iga.korneta@gmail.com>
2015-07-03 13:31:15 +0200

    first commit
    all files

---
+++ server.R
@@ -0,0 +1,129 @@
+# server.R
+# author: Iga Korneta
+# version: 1.0
+# created: June 1st, 2015
+# UN General Assembly Voting Networks
+
+# load the libraries
+library(shiny)
+library(ggplot2)
+library(igraph)
+
+
+#load the tables
+net_all_5 <- read.csv('./data/002_net_all_0.5.csv')
+net_all_7 <- read.csv('./data/002_net_all_0.7.csv')
+net_hr_5 <- read.csv('./data/002_net_hr_0.5.csv')
+net_hr_7 <- read.csv('./data/002_net_hr_0.7.csv')
+net_col_5 <- read.csv('./data/002_net_col_0.5.csv')
+net_col_7 <- read.csv('./data/002_net_col_0.7.csv')
+net_ec_5 <- read.csv('./data/002_net_ec_0.5.csv')
+net_ec_7 <- read.csv('./data/002_net_ec_0.7.csv')
+
+countries <- read.csv('./data/001_countrydata.csv')
+UNRGmap <- c("dark green", "red", "orange", "purple", "dark blue")
+P5.G4.UfCcoremap <- c("orange", "grey", "red", "brown")
+AU.EU.ASEANmap <- c("red", "dark green", "pink", "orange", "dark blue", "yellow", "grey", "purple")
+
+
+# main function
+shinyServer(
+  function(input, output) {
+    
+    ###picking the right table
+    net <- reactive({
+      if (input$restype=='All' & input$abstvote=="0.5") {net_all_5}
+      else if (input$restype=='All' & input$abstvote=="0.7") {net_all_7}
+      else if (input$restype=='Human Rights (sess. 58-66)' & input$abstvote=="0.5") {net_hr_5}
+      else if (input$restype=='Human Rights (sess. 58-66)' & input$abstvote=="0.7") {net_hr_7}
+      else if (input$restype=='Colonialism (sess. 58-66)' & input$abstvote=="0.5") {net_col_5}
+      else if (input$restype=='Colonialism (sess. 58-66)' & input$abstvote=="0.7") {net_col_7}
+      else if (input$restype=='Economy (sess. 58-66)' & input$abstvote=="0.5") {net_ec_5}
+      else if (input$restype=='Economy (sess. 58-66)' & input$abstvote=="0.7") {net_ec_5}
+    })   
+    
+    
+    ###creating the amended network
+    new_weight <- reactive({ifelse(net()$sim > quantile(net()$sim, as.numeric(input$cutoff)), net()$sim, 0)})
+    min_new_weight <- reactive({min(new_weight()[new_weight()>0])})
+    max_new_weight <- reactive({max(new_weight()[new_weight()>0])})
+    new_weight_2 <- reactive({ifelse(new_weight()==0, 0, (new_weight()+1-min_new_weight())/(max_new_weight()-min_new_weight()))})
+    
+    net_new <- reactive({data.frame(cbind(net()[,1:2], sim=new_weight_2()))})
+    net_new2 <- reactive({net_new()[net_new()$sim!=0,]})
+    
+    
+    ###creating the colormap
+    color <- reactive({switch(input$color, 
+                                  "UN Regional Groups"= countries$UNRG, 
+                                  "P5/G4/UfC (core countries only)"= countries$P5.G4.UfCcore,
+                                  "AU/EU/ASEAN/CIS/UNASUR/CARICOM/GCC" = countries$AU.EU.ASEAN)})
+
+    map <- reactive({switch(input$color, 
+                                "UN Regional Groups"= UNRGmap, 
+                                "P5/G4/UfC (core countries only)"= P5.G4.UfCcoremap,
+                                "AU/EU/ASEAN/CIS/UNASUR/CARICOM/GCC" = AU.EU.ASEANmap)})
+    
+    color2 <- reactive({map()[color()]})
+    
+    countries_new <- reactive({data.frame(cbind(countries, color=color2()))})
+
+    
+    #####visualising
+    unscg <- reactive({graph.data.frame(net_new2(), vertices=countries_new(), directed=FALSE)})
+    unscg_small <- reactive({induced.subgraph(unscg(), V(unscg())[degree(unscg())>0])})
+  
+    tempsize <- reactive({switch(input$small, "Yes"= unscg(), "No"= unscg_small())})
+  
+    
+    unscg_p <- reactive({plot.igraph(tempsize(), layout=layout.fruchterman.reingold, vertex.size=4, vertex.color= V(tempsize())$color, vertex.label.cex=0.7, vertex.label.family='mono', vertex.label.dist=0.25, vertex.label.color=V(tempsize())$color, edge.width=E(tempsize())$sim, margin=-2)})    
+    output$unscgPlot <- renderPlot({unscg_p()})
+    
+    
+    ###calculating quant properties
+    #####degree
+    unscg_degree <- reactive({degree(unscg())})
+    ord_unscg_degree <- reactive({order(unscg_degree(), decreasing=TRUE)})
+    
+    #####betweenness
+    unscg_bet<- reactive({betweenness(unscg(), normalized=TRUE)})
+    ord_unscg_bet <- reactive({order(unscg_bet(), decreasing=TRUE)})
+    
+    ####communities
+    fc <- reactive({fastgreedy.community(unscg())})
+    fc_siz <- reactive({sizes(fc())})
+    
+    ####assortativity
+    ass_UNRG <- reactive({assortativity(unscg(), countries$UNRG)})
+    ass_unions <- reactive({assortativity(unscg(), countries$AU.EU.ASEAN)})
+    
+    ###endresults
+    output$unscgQuant0 <- renderText({switch(input$quantproperties,
+                                    "Nodes with highest degree"="Nodes with highest degrees:", 
+                                    "Nodes with highest betweenness"="Nodes with highest betweenness:", 
+                                    "Communities"="Sizes of >1-member communities and some of the members of the largest community:", 
+                                    "Assortativity"="Assortativity wrt UNRG (top) and different regional unions (bottom):"
+                                    )
+                             })
+    
+    
+    output$unscgQuant1 <- renderPrint({switch(input$quantproperties,
+                "Nodes with highest degree"=head(unscg_degree()[ord_unscg_degree()]), 
+                "Nodes with highest betweenness"=head(unscg_bet()[ord_unscg_bet()]), 
+                "Communities"=fc_siz()[fc_siz()>1], 
+                "Assortativity"=ass_UNRG()
+    )     
+    }, width=30)
+      
+    
+    output$unscgQuant2 <- renderPrint({switch(input$quantproperties,
+               "Nodes with highest degree"=head(countries[ord_unscg_degree(),c(1,2)]), 
+               "Nodes with highest betweenness"=head(countries[ord_unscg_bet(),c(1,2)]), 
+               "Communities"=head(countries[membership(fc())==1, c(1,2)]), 
+               "Assortativity"=ass_unions()
+    )     
+  })  
+
+  
+  }
+)
---
+++ ui.R
@@ -0,0 +1,94 @@
+# ui.R
+# author: Iga Korneta
+# version: 1.0
+# created: June 1st, 2015
+# UN General Assembly Voting Networks
+
+library(shiny)
+
+shinyUI(fluidPage(
+  titlePanel("UN General Assembly Voting Networks (sessions 58-67 cumulative)"),  
+  fluidRow(
+    column(3,
+           wellPanel(
+             p(strong("Visualise the UNGA Voting Networks.")),
+             br(),
+             p(strong("Author: "), a(href="mailto:iga.korneta@gmail.com", "Iga Korneta")),
+             p(strong("Date: "), "July 2015"),
+             p(strong("Code: "), a(href="http://github.com/ikorneta/unscapp", "Github")),
+             p(strong("Data source:"), a(href="https://dataverse.harvard.edu/dataset.xhtml?persistentId=hdl:1902.1/12379", "Erik Voeten Dataverse")),
+             p("See the", strong("Help"), "tabs for the explanation of how I selected the data for the visualisation, as well as the various options.")
+           )),
+    
+    column(9,
+           tabsetPanel(
+             tabPanel("Output",
+                      column(8,
+                             p(strong("Voting Networks Graph")),
+                             plotOutput("unscgPlot"),
+                             br()
+                      ),
+                      column(4,
+                             p(strong("Network Properties")),
+                             br(),
+                             textOutput("unscgQuant0"),
+                             verbatimTextOutput("unscgQuant1"),
+                             verbatimTextOutput("unscgQuant2")
+                      )             
+             ),
+             tabPanel("Help - data",
+                      column(12, 
+                             p(strong("Data")),
+                             p("The visualised data come from the results of the non-unanimous UN GA votes collected in the", a(href="https://dataverse.harvard.edu/dataset.xhtml?persistentId=hdl:1902.1/12379", "Eric Voeten Dataverse"), ". Resolutions are classified as described in ", a(href="http://ssrn.com/abstract=2111149", "Voeten, E. (2012)"), ". For resolution subtypes, I chose to use only subtypes with the largest resolving power according to (Voeten, 2012)."),
+                             p("I chose to use only data from after 2004 (inclusive), i.e. sessions 58-67."),
+                             p("The networks are constructed as follows:"),
+                             p("- for each agreement in votes, +1 point is given, for each vote in which one country had a Yes/No vote and the other abstained, either +0.5 or +0.7 points (this is the option ", strong("Abstention vote weight"), ") are given;"),
+                             p("- edges in the quantiles below the cutoff (e.g. the lowest 95%) are dropped (this is the option ", strong("Edge quantile cutoff"), "). Basically, the larger this value, the more edges are dropped.")
+                )), 
+             tabPanel("Help - visualisation",
+                      column(12,
+                             p(strong("Visualisation options")),
+                             p("You can hide lone nodes with the option ", strong("Show zero-degree nodes"), "."),
+                             p("You can ", strong("colour"), " the countries by ", a(href="http://www.un.org/depts/DGACM/RegionalGroups.shtml", "UN Regional Groups"), ", by whether they are in the P5, G4 or in the UfC core (as defined ", a(href="https://en.wikipedia.org/wiki/Uniting_for_Consensus", "here"),"), or by membership in the various regional unions. Membership in UNASUR for some Central American countries is superceded by their earlier membership in CARICOM."),
+                             br(),
+                             p("A fun thing to do is to slide the cutoff from the very left to the very right. Remember to switch on/off the zero-degree nodes!")
+                             
+                             )                  
+                      ),
+             tabPanel("Help - properties",
+                      column(12,
+                             p(strong("Quantitative properties")),
+                             p("Nodes with the ", strong("highest degree"), " have the most connections."),
+                             p("Nodes with the ", strong("highest betweenness"), " are the most crucial to connecting different communities."),
+                             p("Distinct ", strong("communities"), " are tightly-connected subnetworks of the main network."),
+                             p("The ", strong("assortativity"), " coefficient is positive if similar vertices (based on some external property, in this case belonging to the same UN Regional Group or the same regional union) tend to connect to each other, and negative otherwise.")
+                      )                  
+             )             
+             
+             
+             
+             
+           ))
+  ),
+  fluidRow(
+    column(12,
+           p(h4(strong('Pick the options:'))),
+           column(3, 
+                  radioButtons("restype", "Resolution type", choices=c("All", "Human Rights (sess. 58-66)", "Colonialism (sess. 58-66)", "Economy (sess. 58-66)"), selected="All")
+           ),
+           column(3,
+                  radioButtons("abstvote", "Abstention vote weight", choices=c("0.5", "0.7"), selected="0.5", inline=TRUE),
+                  br(),
+                  sliderInput("cutoff", "Edge quantile cutoff", value=0.9, min=0.5, max=0.99, step=0.01)
+           ),
+           column(3,
+                  radioButtons("small", "Show zero-degree nodes", choices=c("Yes", "No"), selected="Yes", inline=TRUE),
+                  br(),
+                  radioButtons("color", "Colour countries", choices=c("UN Regional Groups", "P5/G4/UfC (core countries only)", "AU/EU/ASEAN/CIS/UNASUR/CARICOM/GCC"), selected="UN Regional Groups")
+           ),
+           column(3,
+                  radioButtons("quantproperties", "Quantitative properties", choices=c("Nodes with highest degree", "Nodes with highest betweenness", "Communities", "Assortativity"), selected="Nodes with highest degree")
+           )
+    )
+  )  
+))
